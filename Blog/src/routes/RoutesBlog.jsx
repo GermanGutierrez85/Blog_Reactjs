e7fe7d9ad3aca122274021d225382bd412005535
GermanGutierrez85 <guticen12@gmail.com>
2023-10-20 09:26:58 -0300

    Retoques finales

--- Blog/src/routes/RoutesBlog.jsx
+++ Blog/src/routes/RoutesBlog.jsx
@@ -16,6 +16,7 @@ export default function RoutesBlog() {
       <Router>
         <Navbar />
         <Routes>
+          <Route path="" element={<Home />} />
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/logout" element={<Logout />} />
