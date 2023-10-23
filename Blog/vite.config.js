b0fd9cdd4316248f51f5ebf8829c0d60739bb676
GermanGutierrez85 <guticen12@gmail.com>
2023-10-23 15:08:03 -0300

    pruebas

--- Blog/vite.config.js
+++ Blog/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/blog_reactjs/Blog/",
-  plugins: [react()],
+  plugins: [react()]
 })
